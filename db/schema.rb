d8fddafecac10e9192aed94d0526bf0e46d0388a
Daniel Wangai <mainad57@gmail.com>
2016-10-26 16:15:36 +0300

    mail confirmation works

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20161026113907) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
