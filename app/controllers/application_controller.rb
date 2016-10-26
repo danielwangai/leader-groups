4a0e2799be3310511849ab32061c1b0c8a27353a
Daniel Wangai <mainad57@gmail.com>
2016-10-26 15:28:33 +0300

    added method to solve forbidden attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_filter :forbidden_attributes_error_hack
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Access Denied!!"
     redirect_to root_url
@@ -13,5 +15,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def forbidden_attributes_error_hack
+    return if request.get?
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
   end
 end
