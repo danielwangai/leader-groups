65e0c02c60c4436f4befcca8ae07347263a997fb
Daniel Wangai <mainad57@gmail.com>
2016-10-26 14:40:55 +0300

    added user validation(username). Added devise - views

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,7 @@
 class Role < ApplicationRecord
+  # relationships
   has_many :users
+
+  # validations
   validates :role_name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # relationships
   belongs_to :role
+
+  # validations
+  validates :username, presence: true
 end
