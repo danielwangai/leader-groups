a5bcf5f5069be3d0ba470d4a37d389fa77fa28b1
Daniel Wangai <mainad57@gmail.com>
2016-10-26 15:26:03 +0300

    added flash notice. added access denied message on flash notices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Access Denied!!"
+    redirect_to root_url
+  end
+
   protected
 
   def configure_permitted_parameters
