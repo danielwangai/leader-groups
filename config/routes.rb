8a796f237f98f3a9bac0e758680e8f432b815ec9
Daniel Wangai <mainad57@gmail.com>
2016-10-29 03:38:55 +0300

    added events scaffold and a join table for members and events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   resources :archives
   resources :reports
   resources :members
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027054054) do
+ActiveRecord::Schema.define(version: 20161029003633) do
 
   create_table "archives", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,21 @@ ActiveRecord::Schema.define(version: 20161027054054) do
     t.datetime "book_updated_at"
     t.integer  "archive_id"
     t.index ["archive_id"], name: "index_documents_on_archive_id"
+  end
+
+  create_table "event_members", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_members_on_event_id"
+    t.index ["member_id"], name: "index_event_members_on_member_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "groups", force: :cascade do |t|
@@ -62,6 +77,14 @@ ActiveRecord::Schema.define(version: 20161027054054) do
     t.string   "phone_number"
     t.string   "email"
     t.index ["group_id"], name: "index_members_on_group_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "reports", force: :cascade do |t|
